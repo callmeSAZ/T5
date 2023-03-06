2efc3a01634546d0de107d426fa69ce5f8ff041e
callmeSAZ <121518844+callmeSAZ@users.noreply.github.com>
2023-03-06 20:17:20 +0330

    Add project files.

---
+++ T3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //جدا کردن یکان و دهگان 
+            
+            int x = int.Parse(Console.ReadLine());
+            int y,d;
+
+            y = x % 10;
+            d = x / 10;
+
+            Console.WriteLine("Yekan={0}\nDahgan={1}",y,d);
+
+            Console.ReadKey();
+        }
+    }
+}
